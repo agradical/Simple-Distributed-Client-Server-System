39f3296093f97e8e86699ffdbefe93d7d6b03ac1
akkig <ankur.gpt28@gmail.com>
2016-02-27 19:58:06 -0600

    write operation

--- src/utd/aos/utils/Operations.java
+++ src/utd/aos/utils/Operations.java
@@ -2,9 +2,13 @@ package utd.aos.utils;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.RandomAccessFile;
 import java.io.Serializable;
 
 public class Operations implements Serializable{
@@ -65,7 +69,7 @@ public class Operations implements Serializable{
 				isr.read(result, resource.getSeek(), count);
 				
 				m.setStatusCode(200);
-				m.setMesssage(result.toString());
+				m.setMesssage(new String(result));
 				isr.close();
 				
 			} catch (FileNotFoundException f) {
@@ -78,7 +82,54 @@ public class Operations implements Serializable{
 		}
 		
 		else if(this.operation.equals(OperationMethod.WRITE)) {
+			File originalFile = new File(DATADIRECTORY, resource.getFilename());
+			File file =  new File(DATADIRECTORY, "."+resource.getFilename()+".tmp");
+			if(!file.exists()) {
+				InputStream inStream = null;
+				OutputStream outStream = null;
+				try {
+					file.createNewFile();
+					
+		    	    inStream = new FileInputStream(originalFile);
+		    	    outStream = new FileOutputStream(file);
+		        	
+		    	    byte[] buffer = new byte[1024];
+		    		
+		    	    int length;
+		    	    //copy the file content in bytes 
+		    	    while ((length = inStream.read(buffer)) > 0){    	  
+		    	    	outStream.write(buffer, 0, length);
+		    	    }
+		    	    inStream.close();
+		    	    outStream.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					m.setStatusCode(100);
+					m.setMesssage("File cannot be written");
+					e.printStackTrace();
+				}
+			} else {
+				m.setStatusCode(100);
+				m.setMesssage("Previous operation not comitted yet");
+			}
 			
+			if(originalFile.exists() && file.exists()) {
+				try{
+					RandomAccessFile file_r = new RandomAccessFile(file, "w");
+					file_r.seek(resource.getSeek());
+					byte[] ip_bytes = this.getArg().getBytes();
+					file_r.write(ip_bytes);
+					file_r.close();
+				} catch (FileNotFoundException e) {
+					m.setStatusCode(100);
+					m.setMesssage("File not Found");
+					e.printStackTrace();
+				} catch (IOException i) {
+					m.setStatusCode(100);
+					m.setMesssage("File not Found");
+					i.printStackTrace();
+				}
+			}
 		}
 		
 		else if(this.operation.equals(OperationMethod.DELETE)) {
